62b067b24b505869c6f0af9f9060b2c1e2eca034
Prabin Moktan <prabinmoktan@Prabins-MacBook-Air-2.local>
2025-03-12 18:27:58 +0545

    ignore list changed

--- backend/src/routes/cart.route.ts
+++
@@ -1,8 +0,0 @@
-import  express from 'express';
-import { addToCart } from '../controllers/cart.controller';
-
-const router = express.Router();
-
-router.route('/cart').post(addToCart)
-
-export default router;
--- backend/src/routes/category.route.ts
+++
@@ -1,9 +0,0 @@
-import  express  from "express"
-import { createCategory, getCategory } from "../controllers/category.controller";
-
-const router = express.Router();
-
-router.post('/category',createCategory)
-router.route('/category').get(getCategory);
-
-export default router;

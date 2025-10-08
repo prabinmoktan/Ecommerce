d25a34e0c4d204ee11bab00530a59b60028b36aa
Prabin Moktan <prabinmoktan07@gmail.com>
2025-10-08 18:25:32 +0300

    build error issue fixed with some pages created

--- backend/dist/App.js
+++ backend/dist/App.js
@@ -15,7 +15,7 @@ app.use((0, cors_1.default)({
         'http://localhost:5173',
         'http://localhost:5173/admin',
         'https://ecommerce-y8ix.onrender.com',
-        'https://ecommerce-ashen-two-75.vercel.app'
+        'https://https://ecommerce-blush-three-79.vercel.app'
     ],
     credentials: true,
 }));
--- backend/dist/constant.js
+++ backend/dist/constant.js
@@ -1,1 +1,4 @@
-export const DB_NAME = "Ecommerce";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DB_NAME = void 0;
+exports.DB_NAME = "Ecommerce";
